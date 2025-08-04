e10cf51ca624687238524ae47e80cd45f7f4b77c
Dohun Kim <dohun8758@naver.com>
2025-08-04 18:53:45 +0900

    feat: initial project setup

---
+++ src/main/java/redot/redot_server/RedotServerApplication.java
@@ -0,0 +1,12 @@
+package redot.redot_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedotServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedotServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/redot/redot_server/RedotServerApplicationTests.java
@@ -0,0 +1,13 @@
+package redot.redot_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedotServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
