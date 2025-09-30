66e641195a5d3a5d72978738ef9e4c7090158a4b
Dohun Kim <dohun8758@naver.com>
2025-09-30 16:56:35 +0900

    feat: 고객사 CMSMember Entity 생성

---
+++ src/main/java/redot/redot_server/domain/cms/entity/CMSMember.java
@@ -0,0 +1,79 @@
+package redot.redot_server.domain.cms.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Builder(access = AccessLevel.PRIVATE)
+@Table(
+        name = "cms_members",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"customer_id", "email"})
+        }
+)
+public class CMSMember {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id", nullable = false)
+    @Getter
+    private Customer customer;
+
+    @Column(nullable = false)
+    @Getter
+    private String name;
+
+    @Column(nullable = false)
+    @Getter
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    @Getter
+    private CMSMemberRole role;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    public boolean belongsTo(Customer customer) {
+        return this.customer.equals(customer);
+    }
+
+    public static CMSMember create(Customer customer, String name, String email, String password, CMSMemberRole role) {
+        return CMSMember.builder()
+                .customer(customer)
+                .name(name)
+                .email(email)
+                .password(password)
+                .role(role)
+                .build();
+    }
+}
