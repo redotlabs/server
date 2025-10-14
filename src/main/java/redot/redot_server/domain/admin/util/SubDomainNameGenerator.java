623c783a44391a37951d850a4cecdbf3fcd7cdb5
Dohun Kim <dohun8758@naver.com>
2025-10-14 12:17:28 +0900

    feat: 도메인 생성 유틸에서 subdomain만 반환하도록 변경
    - header 로직의 변경 예정으로 인한 도메인 저장 방식 변경
      - full domain(asdjas12.redot.me) -> subdomain(asdjas12)

--- src/main/java/redot/redot_server/domain/admin/util/SubDomainNameGenerator.java
+++ src/main/java/redot/redot_server/domain/admin/util/SubDomainNameGenerator.java
@@ -2,20 +2,18 @@ package redot.redot_server.domain.admin.util;
 
 import java.security.SecureRandom;
 
-public class DomainNameGenerator {
+public class SubDomainNameGenerator {
 
     private static final int SUBDOMAIN_LENGTH = 8;
     private static final String CHARACTERS = "abcdefghijklmnopqrstuvwxyz0123456789";
     private static final SecureRandom RANDOM = new SecureRandom();
-    private static final String DOMAIN_SUFFIX = ".redot.me";
 
     public static String generateSubdomain() {
-        StringBuilder domainName = new StringBuilder(SUBDOMAIN_LENGTH + DOMAIN_SUFFIX.length());
+        StringBuilder domainName = new StringBuilder(SUBDOMAIN_LENGTH);
         for (int i = 0; i < SUBDOMAIN_LENGTH; i++) {
             int index = RANDOM.nextInt(CHARACTERS.length());
             domainName.append(CHARACTERS.charAt(index));
         }
-        domainName.append(DOMAIN_SUFFIX);
         return domainName.toString();
     }
 }
