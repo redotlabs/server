24eaf6d90387fd57aba153011c68dbf627222f02
Dohun Kim <dohun8758@naver.com>
2025-10-25 13:30:46 +0900

    deploy: 테스트 서버 배포 관련 문제 해결

--- src/main/java/redot/redot_server/global/jwt/cookie/CookieProviderDev.java
+++ src/main/java/redot/redot_server/global/jwt/cookie/CookieProviderDev.java
@@ -21,9 +21,8 @@ public class CookieProviderDev implements CookieProvider{
 
     private String resolveDomain(HttpServletRequest request) {
         System.out.println(request.getHeader("X-Forwarded-Host"));
-        System.out.println(request.getHeader("X-Original-Host"));
         System.out.println(request.getHeader("Host"));
-        String host = request.getHeader("X-Original-Host");
+        String host = request.getHeader("Host");
         if (host.contains("localhost")) return "localhost";
         return ".redotlabs.vercel.app";
     }
