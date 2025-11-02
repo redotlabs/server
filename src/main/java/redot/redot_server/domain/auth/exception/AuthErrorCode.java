4e936d4f294ede11b17665cd1183dadf7ddcfc73
Dohun Kim <dohun8758@naver.com>
2025-11-02 23:50:44 +0900

    feat: admin 조회, 추가, 삭제 API 구현

--- src/main/java/redot/redot_server/domain/auth/exception/AuthErrorCode.java
+++ src/main/java/redot/redot_server/domain/auth/exception/AuthErrorCode.java
@@ -22,6 +22,7 @@ public enum AuthErrorCode implements ErrorCode {
     ADMIN_NOT_FOUND(404, 1012, "관리자 계정을 찾을 수 없습니다."),
     CMS_MEMBER_NOT_FOUND(404, 1013, "CMS 회원을 찾을 수 없습니다."),
     EMAIL_ALREADY_EXISTS(400, 1014, "이미 존재하는 이메일입니다.");
+    CANNOT_DELETE_OWN_ADMIN_ACCOUNT(400, 1015, "자신의 관리자 계정은 삭제할 수 없습니다."),
 
     private final int statusCode;
     private final int exceptionCode;
