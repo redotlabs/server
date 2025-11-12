7b3c56f8d1b6ccc19d51aa41f8eb3bcbfb5019ba
Dohun Kim <dohun8758@naver.com>
2025-11-12 19:59:18 +0900

    fix(cms): correct typo in error message for owner account deletion

--- src/main/java/redot/redot_server/domain/cms/exception/CMSMemberErrorCode.java
+++ src/main/java/redot/redot_server/domain/cms/exception/CMSMemberErrorCode.java
@@ -11,7 +11,7 @@ public enum CMSMemberErrorCode implements ErrorCode {
     CMS_MEMBER_ROLE_UNCHANGED(400, 6001, "변경할 CMS 멤버의 역할이 기존과 동일합니다."),
     CMS_MEMBER_ALREADY_DELETED(400, 6002, "이미 삭제된 CMS 멤버입니다."),
     CANNOT_DELETE_SELF(400, 6003, "본인 계정은 삭제할 수 없습니다."),
-    CANNOT_DELETE_OWNER(400, 6004, "고객사의 owner 계정은 삭제하거나 탈퇴할 없습니다.");
+    CANNOT_DELETE_OWNER(400, 6004, "고객사의 owner 계정은 삭제하거나 탈퇴할 수 없습니다.");
 
     private final int statusCode;
     private final int exceptionCode;
