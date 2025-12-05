16d60b7cd6646ed674e208d6e5605a81f0b3f3eb
Dohun Kim <dohun8758@naver.com>
2025-12-05 19:26:56 +0900

    feat(consultation): add validation annotations to consultation request DTOs and update controller methods

--- src/main/java/redot/redot_server/domain/redot/consultation/exception/ConsultationException.java
+++ src/main/java/redot/redot_server/domain/redot/consultation/exception/ConsultationException.java
@@ -1,6 +1,5 @@
 package redot.redot_server.domain.redot.consultation.exception;
 
-import redot.redot_server.domain.site.domain.exception.DomainErrorCode;
 import redot.redot_server.global.exception.BaseException;
 
 public class ConsultationException extends BaseException {
