07edfb416ddc933eb37cd2d9ebc1f36e420efa3e
Dohun Kim <dohun8758@naver.com>
2025-12-07 12:38:52 +0900

    refactor(image-upload): implement image upload service and related utilities

---
+++ src/main/java/redot/redot_server/global/s3/service/ImageUploadService.java
@@ -0,0 +1,26 @@
+package redot.redot_server.global.s3.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import redot.redot_server.global.s3.exception.ImageErrorCode;
+import redot.redot_server.global.s3.exception.ImageUploadException;
+import redot.redot_server.global.s3.util.ImageDirectory;
+import redot.redot_server.global.s3.util.ImagePathGenerator;
+import redot.redot_server.global.s3.util.S3Manager;
+
+@Service
+@RequiredArgsConstructor
+public class ImageUploadService {
+
+    private final S3Manager s3Manager;
+
+    public String upload(ImageDirectory directory, Long ownerId, MultipartFile file) {
+        if (file == null || file.isEmpty()) {
+            throw new ImageUploadException(ImageErrorCode.IMAGE_FILE_REQUIRED);
+        }
+
+        String path = ImagePathGenerator.generate(directory, ownerId, file.getOriginalFilename());
+        return s3Manager.uploadFile(file, path);
+    }
+}
---
+++ src/main/java/redot/redot_server/global/s3/util/ImageDirectory.java
@@ -0,0 +1,20 @@
+package redot.redot_server.global.s3.util;
+
+import java.util.function.Function;
+
+public enum ImageDirectory {
+    ADMIN_PROFILE(id -> String.format("admin/profile/%d", id)),
+    CMS_MEMBER_PROFILE(id -> String.format("cms/member/profile/%d", id)),
+    REDOT_MEMBER_PROFILE(id -> String.format("redot/member/profile/%d", id)),
+    APP_LOGO(id -> String.format("app/%d/logo", id));
+
+    private final Function<Long, String> pathResolver;
+
+    ImageDirectory(Function<Long, String> pathResolver) {
+        this.pathResolver = pathResolver;
+    }
+
+    public String resolve(Long ownerId) {
+        return pathResolver.apply(ownerId);
+    }
+}
