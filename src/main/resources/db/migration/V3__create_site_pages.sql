583261882151f1cac208751d0ed628b1748dc09c
Dohun Kim <dohun8758@naver.com>
2025-12-08 22:48:21 +0900

    feat(database): create tables for managing site pages and versions

---
+++ src/main/resources/db/migration/V3__create_site_pages.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTS app_pages
+(
+    id           BIGSERIAL PRIMARY KEY,
+    redot_app_id BIGINT        NOT NULL REFERENCES redot_apps (id),
+    content      TEXT          NOT NULL,
+    path         VARCHAR(255)  NOT NULL,
+    is_protected BOOLEAN       NOT NULL,
+    description  TEXT,
+    title        VARCHAR(100)  NOT NULL,
+    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS app_versions
+(
+    id               BIGSERIAL PRIMARY KEY,
+    redot_app_id     BIGINT       NOT NULL REFERENCES redot_apps (id),
+    remark           TEXT,
+    status           VARCHAR(50)  NOT NULL,
+    published_app_id BIGINT UNIQUE REFERENCES redot_apps (id),
+    created_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+ALTER TABLE app_versions
+    ADD CONSTRAINT ck_app_versions_status CHECK (status IN ('PUBLISHED', 'DRAFT', 'PREVIOUS'));
+
+CREATE TABLE IF NOT EXISTS app_version_pages
+(
+    id             BIGSERIAL PRIMARY KEY,
+    app_version_id BIGINT NOT NULL REFERENCES app_versions (id) ON DELETE CASCADE,
+    app_page_id    BIGINT NOT NULL REFERENCES app_pages (id) ON DELETE CASCADE,
+    created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT uk_app_version_page UNIQUE (app_version_id, app_page_id)
+);
